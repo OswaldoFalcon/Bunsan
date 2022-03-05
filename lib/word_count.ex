07613452c4956c9a90ce05a51cd4ff1f1ab3d6c8
oswaldo falcon reyes <oswaldofalconreyes@MacBook-Air-de-oswaldo.local>
2022-03-05 00:25:38 -0600

    Primera version de modulo word_count

---
+++ lib/word_count.ex
@@ -0,0 +1,10 @@
+defmodule WordCount do
+    def count(path) do
+    {:ok, text} = File.read(path)
+    #se usa Sigils para poder elinar los caracteres especiales, pero mantener letras y numeros#
+    list_words = String.downcase(text) |> String.normalize(:nfd) |> String.replace(~r/[^0-9A-z\s]/u, "") |> String.split(" ")
+    # Ahora el  acumulador es un mapa y vamos agregando llaves 
+    list_words|> Enum.reduce(%{}, fn palabra, acc_mapa -> Map.update(acc_mapa, palabra, 1, fn palabra -> palabra + 1 end) end)
+    end
+end
+
